5b4c23120a773798e7120e8fdd65d79cb4fe3ab7
Nagarajan Sethu <letsmailnagarajan@gmail.com>
2024-02-22 16:55:35 +0530

    404 page updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div class='min-h-screen flex flex-grow items-center justify-center bg-gray-50'>
-      <div class='rounded-lg bg-white p-8 text-center shadow-xl'>
-        <h1 class='mb-4 text-4xl font-bold'>404</h1>
-        <p class='text-gray-600'>Oops! The page you are looking for could not be found.</p>
+    <div className='min-h-screen flex flex-grow items-center justify-center bg-gray-50'>
+      <div className='rounded-lg bg-white p-8 text-center shadow-xl'>
+        <h1 className='mb-4 text-4xl font-bold'>404</h1>
+        <p className='text-gray-600'>Oops! The page you are looking for could not be found.</p>
         <a href='/products' className='group rounded-lg border border-transparent px-5 py-4 transition-colors'>
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`mb-3 text-md font-semibold`}>
             Go back to Product{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
               -&gt;
