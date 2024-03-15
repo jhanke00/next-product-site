8f0e9f3b37c7fb1885f56ad33f86916547e26465
Pooranachandiran <poorna@LTIN470457.cts.com>
2024-03-15 10:57:21 +0530

    product listings

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+import largeData from '@/src/mock/large/products.json';
+import { useRouter } from 'next/navigation';
+
+const productDetail = ({ params }: { params: { productId: string } }) => {
+  const data = [...largeData];
+  let product;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id === params.productId) {
+      product = data[i];
+      break;
+    }
+  }
+  const router = useRouter();
+  if (!product) {
+    return <p>No products are active now</p>;
+  }
+
+  return (
+    <div className={`flex min-h-screen flex-col p-24`}>
+      <h3 className={`mb-3 text-2xl font-semibold `}>{product.name}</h3>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Price: {product.price}</p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Description: {product.description}</p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Category: {product.category}</p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Rating: {Math.round(product.rating)}</p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Reviews: {product.numReviews}</p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Stock: {product.countInStock}</p>
+      <button className={`pt-4 underline`} onClick={() => router.back()}>
+        Navigate to Main Page
+      </button>
+    </div>
+  );
+};
+
+export default productDetail;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,78 +1,51 @@
 import type { Product } from '@type/products';
+('use client');
+import largeData from '@/src/mock/large/products.json';
+import { useState } from 'react';
+import Link from 'next/link';
+
+const sku_per_page = 10;
 
 export default function Products() {
-  const products: Array<Product> = [
-    {
-      id: 1,
-      name: 'Product 1',
-      price: 100,
-      description: 'This is a product',
-      category: 'Category 1',
-      rating: 4.5,
-      numReviews: 10,
-      countInStock: 6,
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      price: 200,
-      description: 'This is a product',
-      category: 'Category 2',
-      rating: 4.0,
-      numReviews: 10,
-      countInStock: 6,
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      price: 300,
-      description: 'This is a product',
-      category: 'Category 3',
-      rating: 3.5,
-      numReviews: 10,
-      countInStock: 6,
-    },
-    {
-      id: 4,
-      name: 'Product 4',
-      price: 400,
-      description: 'This is a product',
-      category: 'Category 4',
-      rating: 3.0,
-      numReviews: 10,
-      countInStock: 6,
-    },
-    {
-      id: 5,
-      name: 'Product 5',
-      price: 500,
-      description: 'This is a product',
-      category: 'Category 5',
-      rating: 2.5,
-      numReviews: 10,
-      countInStock: 6,
-    },
-  ];
+  const [currentPage, setCurrentPage] = useState(1);
+  const data = [...largeData];
+  const startPos = (currentPage - 1) * sku_per_page;
+  const endPos = startPos + sku_per_page;
+  const skuData = data.slice(startPos, endPos);
+  const totalPages = Math.floor(data.length / sku_per_page);
+
+  const nextPage = () => setCurrentPage(currentPage + 1);
+
+  const prevPage = () => setCurrentPage(currentPage - 1);
 
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <div className='grid lg:max-w-5xl lg:w-full lg:grid-cols-2 lg:text-left'>
-          {products.map((product) => (
+          {skuData.map((product) => (
             <div
               key={product.id}
               className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
             >
-              <h3 className={`mb-3 text-2xl font-semibold`}>{product.name}</h3>
+              <Link href={`/products/${product.id}`}>
+                <h3 className={`mb-3 text-2xl font-semibold underline`}>{product.name}</h3>
+              </Link>
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Price: {product.price}</p>
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Description: {product.description}</p>
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Category: {product.category}</p>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Rating: {product.rating}</p>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Rating: {Math.round(product.rating)}</p>
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Reviews: {product.numReviews}</p>
               <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Stock: {product.countInStock}</p>
             </div>
           ))}
         </div>
+      </div>
+      <div className='flex w-full border-t-2 pt-4' style={{ justifyContent: 'space-evenly' }}>
+        {currentPage !== 1 && <button onClick={prevPage}> Previous </button>}
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        {currentPage !== totalPages && <button onClick={nextPage}> Next </button>}
       </div>
     </main>
   );
