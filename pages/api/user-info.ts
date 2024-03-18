f128af76255c7380854fc3bb98cf201658a34ecd
Shafi Masoumi <masoumishafi@gmail.com>
2024-03-17 17:38:09 -0700

    Enpoint that returns user information

--- pages/api/user-info.ts
+++ pages/api/user-info.ts
@@ -1,2 +1,14 @@
 // User API Endpoint for Small Dataset #14
 // Endpoint that returns the user information
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import usersData from '../../src/mock/small/users.json';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+  // Assuming there's only one user in the mock data
+  const userInfo = usersData[0];
+  res.status(200).json(userInfo);
+}
