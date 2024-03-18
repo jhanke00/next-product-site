7530e03ec3cdef1a9e32b10664d03326a2d192ff
Shafi Masoumi <masoumishafi@gmail.com>
2024-03-17 18:10:34 -0700

    git the specific user by providing the user in header

--- pages/api/user-info.ts
+++ pages/api/user-info.ts
@@ -8,7 +8,26 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
+
+  if (!Array.isArray(usersData) || usersData.length === 0) {
+    console.error('User data not found');
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+
+  const userId = req.headers['user-id']; // Assuming the header is named 'user-id'
+
+  if (!userId) {
+    return res.status(400).json({ message: 'User ID not provided in the header' });
+  }
+
+  // Find the user with the provided ID
+  const user = usersData.find((user) => user.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
   // Assuming there's only one user in the mock data
-  const userInfo = usersData[0];
-  res.status(200).json(userInfo);
+  // const userInfo = usersData[0];
+  res.status(200).json(user);
 }
