065d802fb2d92aba4bf84dc5c73d0ff7211be1e4
Shafi Masoumi <masoumishafi@gmail.com>
2024-03-17 19:02:37 -0700

    endpoint that returns how much the user has spent on orders

--- pages/api/total-spent.ts
+++ pages/api/total-spent.ts
@@ -1,2 +1,29 @@
 // User API Endpoint for Small Dataset #14
-// Endpoint that returns how much the user has spent on orders
+// Endpoint that returns the user information
+import { NextApiRequest, NextApiResponse } from 'next';
+import ordersData from '../../src/mock/small/orders.json';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  // const { userId } = req.query;
+  const userId = req.headers['user-id'];
+
+  if (!userId) {
+    return res.status(400).json({ message: 'User ID is required' });
+  }
+
+  // Filter orders for the specified user ID
+  const userOrders = ordersData.filter((order) => order.user === userId);
+
+  if (userOrders.length === 0) {
+    return res.status(404).json({ message: 'No orders found for the user' });
+  }
+
+  // Calculate total spent by the user
+  const totalSpent = userOrders.reduce((total, order) => total + order.total, 0);
+
+  res.status(200).json({ totalSpent });
+}
